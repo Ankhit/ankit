b42d01daca393a80246fabf9f52e8c6a68bdd9b6
Ankhit <tankhit535@gmail.com>
2022-03-08 22:36:57 +0100

    Initial commit

---
+++ src/test/java/TestAdd.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestAdd {
+
+    private static Add uc;
+
+    @BeforeClass
+    public static void setup() {
+        uc =new Add();
+    }
+
+    @Test
+    public void test_add() {
+        assertEquals(4, uc.add(2,2));
+    }
+}
